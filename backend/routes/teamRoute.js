f77c8831b3f605398106f8f442b12d0341d7bdc6
Rudra <rud28b@gmail.com>
2025-12-27 14:06:15 +0530

    fetch all teams added

--- backend/routes/teamRoute.js
+++ backend/routes/teamRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createTeam,
   addMemberToTeam,
+  getAllTeams,
 } from "../controllers/teamController.js";
 
 const router = express.Router();
@@ -11,5 +12,8 @@ router.post("/", createTeam);
 
 // Add member to team
 router.post("/add-member", addMemberToTeam);
+
+// get all teams
+router.get("/", getAllTeams);
 
 export default router;
